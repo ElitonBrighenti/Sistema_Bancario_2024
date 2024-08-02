60223a3bb9e9f8b76eb9e1666e9bc3d63df49a93
Eliton Brighenti <elitonbrighenti09@outlook.com>
2024-08-01 22:05:40 -0300

    Adicionar arquivos de projeto.

---
+++ Sistema_Bancario/ContaBancaria.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Bancario
+{
+    public class ContaBancaria
+    {
+        public int Numero {  get; set; }
+        public string Dono { get; set; }
+        private double saldo;
+
+        public ContaBancaria(int numero, string dono)
+        {
+            Numero = numero;
+            Dono = dono;
+        }
+        public void Sacar(double valor)
+        {
+            if (valor > 0)
+                Console.WriteLine(valor < saldo ? $"Saque realizado com sucesso {saldo -= valor}" : $"Saldo insuficiente");
+        }
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+        public void ConsultarSaldo()
+        {
+            Console.WriteLine($"Saldo atual: {saldo}");
+        }
+    }
+}
---
+++ Sistema_Bancario/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Sistema_Bancario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o numero da conta: ");
+            int num = int.Parse(Console.ReadLine()!);
+            Console.Write("Digite o nome do usuario: ");
+            string nome = Console.ReadLine()!;
+            ContaBancaria conta = new ContaBancaria(num, nome);
+
+            while (true)
+            {
+                Console.WriteLine("1 - Depositar");
+                Console.WriteLine("2 - Sacar");
+                Console.WriteLine("3 - Consultar saldo");
+                Console.WriteLine("0 - Sair");
+                int opcao = int.Parse(Console.ReadLine()!);
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Write($"Diga o valor a depositar: ");
+                        double valor = double.Parse(Console.ReadLine()!);
+                        conta.Depositar(valor);
+                        break;
+                    case 2:
+                        Console.Write($"Digite o valor a sacar: ");
+                        double valor2 = double.Parse(Console.ReadLine()!);
+                        conta.Sacar(valor2);
+                        break;
+                    case 3:
+                        Console.Write($"Seu saldo é de: {conta.ConsultarSaldo}");
+                        break;
+                    case 0:
+                        Console.Write($"Até logo");
+                        Thread.Sleep(3000);
+                        return;
+                }
+            }
+        }
+    }
+}
